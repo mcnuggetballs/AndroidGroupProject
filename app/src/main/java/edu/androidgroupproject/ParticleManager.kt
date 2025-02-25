968482274655ad803651f63ce93953345202bb0f
JTZQ <fishyweeb@gmail.com>
2025-02-25 22:25:11 +0800

    fixed

--- app/src/main/java/edu/androidgroupproject/ParticleManager.kt
+++ app/src/main/java/edu/androidgroupproject/ParticleManager.kt
@@ -5,274 +5,135 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Matrix
 import android.graphics.Paint
-import edu.androidgroupproject.Collision
-import edu.androidgroupproject.IMAGE
-import edu.androidgroupproject.ImageManager
-import edu.androidgroupproject.PARTICLETYPE
-import edu.androidgroupproject.PaintColor
-import edu.androidgroupproject.ParticleObject
-import edu.androidgroupproject.PlayerInfo
-import java.util.LinkedList
+import edu.androidgroupproject.*
 import kotlin.math.atan2
-import edu.androidgroupproject.R
-import edu.androidgroupproject.ResourceManager
-import edu.androidgroupproject.SHOOTTYPE
+import kotlin.math.PI
+import kotlin.random.Random
 
 class ParticleManager {
-    private val particleList = LinkedList<ParticleObject>()
-    private val particleQueue = LinkedList<ParticleObject>()
+    private val particleList = mutableListOf<ParticleObject>()
+    private val particleQueue = mutableListOf<ParticleObject>()
     private val POWERUP_SPEED = 300
     private var ScreenWidth = 0
     private var ScreenHeight = 0
 
+    fun Init(_ScreenWidth: Int, _ScreenHeight: Int) {
+        ScreenWidth = _ScreenWidth
+        ScreenHeight = _ScreenHeight
+
+        for (i in PARTICLETYPE.values().indices) {
+            repeat(100) {
+                particleList.add(ParticleObject(PARTICLETYPE.values()[i]))
+            }
+        }
+    }
+
     fun FetchParticle(type: PARTICLETYPE): ParticleObject {
-        for (p in particleList) {
-            if (p.active || p.type != type) continue
-            p.active = true
-            return p
+        particleList.firstOrNull { !it.active && it.type == type }?.let {
+            it.active = true
+            return it
         }
 
-        val newParticle = ParticleObject(type)
-        newParticle.active = true
-        particleQueue.add(newParticle)
-
-        return newParticle
+        return ParticleObject(type).apply {
+            active = true
+            particleQueue.add(this)
+        }
     }
 
     fun CreateParticle(type: PARTICLETYPE, x: Float, y: Float) {
         when (type) {
             PARTICLETYPE.P_BUBBLE -> {
                 val newParticle = Instance.FetchParticle(PARTICLETYPE.P_BUBBLE)
-                val diameter = (Math.random() * 50 + 15).toInt()
+                val diameter = Random.nextInt(15, 65)
                 newParticle.width = diameter.toFloat()
                 newParticle.height = diameter.toFloat()
                 newParticle.position.x = x
                 newParticle.position.y = y
-                newParticle.velocity.x = Math.random().toFloat() * 40 - 20
-                newParticle.velocity.y = -(Math.random().toFloat() * 160 + 80)
+                newParticle.velocity.x = Random.nextFloat() * 40 - 20
+                newParticle.velocity.y = -(Random.nextFloat() * 160 + 80)
                 newParticle.SetBMP(
                     Bitmap.createScaledBitmap(
-                        ImageManager.Companion.Instance.GetImage(
-                            IMAGE.I_BUBBLE
-                        )!!, diameter, diameter, true
+                        ImageManager.Instance.GetImage(IMAGE.I_BUBBLE)!!, diameter, diameter, true
                     )
                 )
             }
-
             PARTICLETYPE.P_BLOOD -> {
                 val tempParticle = Instance.FetchParticle(PARTICLETYPE.P_BLOOD)
                 tempParticle.position.x = x
                 tempParticle.position.y = y
                 tempParticle.width = 20f
                 tempParticle.height = 20f
-                tempParticle.velocity.x = Math.random().toFloat() * 500 - 250
-                tempParticle.velocity.y = Math.random().toFloat() * 500 - 250
+                tempParticle.velocity.x = Random.nextFloat() * 300 - 150
+                tempParticle.velocity.y = Random.nextFloat() * 300 - 150
+                tempParticle.paint = Paint().apply {
+                    color = Color.RED
+                    alpha = 255
+                }
+                tempParticle.timer = 0f
             }
-
             PARTICLETYPE.P_FISH -> {
-                val tempParticle2 = Instance.FetchParticle(PARTICLETYPE.P_FISH)
-                tempParticle2.position.x = x
-                tempParticle2.position.y = y
-                tempParticle2.velocity.x = Math.random().toFloat() * 500 - 250
-                tempParticle2.velocity.y = Math.random().toFloat() * 500 - 250
-                tempParticle2.SetBMP(ResourceManager.Companion.Instance.GetBitmap(R.drawable.sprite_fish)!!)
+                val tempParticle = Instance.FetchParticle(PARTICLETYPE.P_FISH)
+                tempParticle.position.x = x
+                tempParticle.position.y = y
+                tempParticle.velocity.x = Random.nextFloat() * 500 - 250
+                tempParticle.velocity.y = Random.nextFloat() * 500 - 250
+                tempParticle.SetBMP(ResourceManager.Instance.GetBitmap(R.drawable.sprite_fish)!!)
             }
-
             else -> {}
         }
     }
 
-    fun Init(_ScreenWidth: Int, _ScreenHeight: Int) {
-        ScreenWidth = _ScreenWidth
-        ScreenHeight = _ScreenHeight
-
-        for (i in 0 until PARTICLETYPE.P_NUM.GetSize()) {
-            for (i2 in 0..99) {
-                particleList.add(ParticleObject(PARTICLETYPE.values()[i]))
-            }
-        }
-    }
-
-    fun LimitVelocity(p: ParticleObject, _limit: Float) {
-        if (p.velocity.x > _limit) p.velocity.x = _limit
-        else if (p.velocity.x < -_limit) p.velocity.x = -_limit
-
-        if (p.velocity.y > _limit) p.velocity.y = _limit
-        else if (p.velocity.y < -_limit) p.velocity.y = -_limit
-    }
-
-    fun DeleteOutOfBounds(p: ParticleObject) {
-        if (p.position.x + (p.width * 0.5f) < 0) {
-            p.active = false
-        } else if (p.position.x - (p.width * 0.5f) > ScreenWidth) {
-            p.active = false
-        }
-
-        if (p.position.y + (p.height * 0.5f) < 0) {
-            p.active = false
-        } else if (p.position.y - (p.height * 0.5f) > ScreenHeight) {
-            p.active = false
-        }
-    }
-
-    fun Update(_dt: Float) {
-        for (newParticle in particleQueue) {
-            particleList.add(newParticle)
-        }
+    fun Update(dt: Float) {
+        particleList.addAll(particleQueue)
         particleQueue.clear()
 
         for (p in particleList) {
             if (!p.active) continue
+            p.position.x += p.velocity.x * dt
+            p.position.y += p.velocity.y * dt
 
-            p.position.PlusEqual(p.velocity, _dt)
-
-            if (p.type == PARTICLETYPE.P_MONEY) {
-                p.target = (PlayerInfo.Companion.Instance.GetPos().Minus(p.position)).Normalized()
-                var DistanceFromPlayer =
-                    p.position.DistanceSquaredFrom(PlayerInfo.Companion.Instance.GetPos())
-                if (DistanceFromPlayer <= 1) DistanceFromPlayer = 1f
-                p.velocity.PlusEqual(p.target.Times(100000000 / DistanceFromPlayer + 10000), _dt)
-
-                LimitVelocity(p, 1000f)
-
-                if (Collision.SphereToSphere(
-                        p.position.x,
-                        p.position.y,
-                        0f,
-                        PlayerInfo.Companion.Instance.GetPos().x,
-                        PlayerInfo.Companion.Instance.GetPos().y,
-                        30f
-                    )
-                ) {
-                    p.active = false
-                    PlayerInfo.Companion.Instance.AddMoney(1)
-                }
-            } else if (p.type == PARTICLETYPE.P_BLOOD) {
-                val friction = 900f
-
-                if (p.velocity.x > friction * _dt) p.velocity.x -= friction * _dt
-                else if (p.velocity.x < -friction * _dt) p.velocity.x += friction * _dt
-
-                if (p.velocity.y > friction * _dt) p.velocity.y -= friction * _dt
-                else if (p.velocity.y < -friction * _dt) p.velocity.y += friction * _dt
-
-                if (p.paint == null) {
-                    p.paint = Paint()
-                    p.paint!!.color = Color.RED
-                    p.paint!!.alpha = 255
-                    p.timer = 0f
-                }
-                p.timer += _dt
+            if (p.type == PARTICLETYPE.P_BLOOD) {
+                p.timer += dt
                 if (p.timer > 0.4) {
                     p.timer = 0f
-                    p.paint!!.alpha = p.paint!!.alpha - 30
+                    p.paint?.alpha = (p.paint?.alpha ?: 255) - 30
                 }
-                if (p.paint!!.alpha < 30) {
-                    p.active = false
-                    p.paint!!.alpha = 255
-                }
-            } else if (p.type == PARTICLETYPE.P_POWERUP_STRAIGHT) {
-                p.velocity.y = POWERUP_SPEED.toFloat()
-                if (Collision.SphereToSphere(
-                        p.position.x,
-                        p.position.y,
-                        0f,
-                        PlayerInfo.Companion.Instance.GetPos().x,
-                        PlayerInfo.Companion.Instance.GetPos().y,
-                        p.width
-                    )
-                ) {
-                    p.active = false
-                    PlayerInfo.Companion.Instance.GetMainWeapon()!!
-                        .AddShootAmount(SHOOTTYPE.S_STRAIGHT)
-                }
-                DeleteOutOfBounds(p)
-            } else if (p.type == PARTICLETYPE.P_POWERUP_SPREAD) {
-                p.velocity.y = POWERUP_SPEED.toFloat()
-                if (Collision.SphereToSphere(
-                        p.position.x,
-                        p.position.y,
-                        0f,
-                        PlayerInfo.Companion.Instance.GetPos().x,
-                        PlayerInfo.Companion.Instance.GetPos().y,
-                        p.width
-                    )
-                ) {
-                    p.active = false
-                    PlayerInfo.Companion.Instance.GetMainWeapon()!!
-                        .AddShootAmount(SHOOTTYPE.S_SPREAD)
-                }
-                DeleteOutOfBounds(p)
-            } else if (p.type == PARTICLETYPE.P_BUBBLE) {
-                p.position.PlusEqual(p.velocity, _dt)
-
-                if (p.position.x + (p.width * 0.5f) < 0) {
-                    p.active = false
-                } else if (p.position.x - (p.width * 0.5f) > ScreenWidth) {
-                    p.active = false
-                }
-                if (p.position.y + (p.height * 0.5f) < 0) {
-                    p.active = false
-                }
-            } else if (p.type == PARTICLETYPE.P_FISH) {
-                p.position.PlusEqual(p.velocity, _dt)
-
-                if (p.position.x + (p.width * 0.5f) < 0) {
-                    p.active = false
-                } else if (p.position.x - (p.width * 0.5f) > ScreenWidth) {
-                    p.active = false
-                }
-                if (p.position.y + (p.height * 0.5f) < 0) {
+                if ((p.paint?.alpha ?: 255) < 30) {
                     p.active = false
                 }
             }
         }
     }
 
-    fun Render(_canvas: Canvas) {
+    fun Render(canvas: Canvas) {
         for (p in particleList) {
             if (!p.active) continue
 
-            if (p.type == PARTICLETYPE.P_MONEY) _canvas.drawOval(
-                p.position.x - p.width * 0.5f,
-                p.position.y - p.height * 0.5f,
-                p.position.x + p.width * 0.5f,
-                p.position.y + p.height * 0.5f,
-                PaintColor.Companion.Instance.GetPaint(Color.YELLOW)!!
-            )
-            else if (p.type == PARTICLETYPE.P_POWERUP_STRAIGHT) _canvas.drawBitmap(
-                p.GetBMP()!!,
-                p.position.x - (p.width * 0.5f),
-                p.position.y - (p.height * 0.5f),
-                null
-            )
-            else if (p.type == PARTICLETYPE.P_POWERUP_SPREAD) _canvas.drawBitmap(
-                p.GetBMP()!!,
-                p.position.x - (p.width * 0.5f),
-                p.position.y - (p.height * 0.5f),
-                null
-            )
-            else if (p.type == PARTICLETYPE.P_BLOOD) _canvas.drawOval(
-                p.position.x - p.width * 0.5f,
-                p.position.y - p.height * 0.5f,
-                p.position.x + p.width * 0.5f,
-                p.position.y + p.height * 0.5f,
-                p.paint!!
-            )
-            else if (p.type == PARTICLETYPE.P_BUBBLE) _canvas.drawBitmap(
-                p.GetBMP()!!,
-                p.position.x - p.width * 0.5f,
-                p.position.y - p.height * 0.5f,
-                null
-            )
-            else if (p.type == PARTICLETYPE.P_FISH) {
-                val transform = Matrix()
-                val dir = p.velocity.Normalized()
-                val rotation =
-                    atan2(dir.y.toDouble(), dir.x.toDouble()).toFloat() * 180 / 3.142f - 90.0f
-                transform.postRotate(rotation)
-                transform.postTranslate(p.position.x, p.position.y)
-                _canvas.drawBitmap(p.GetBMP()!!, transform, null)
+            when (p.type) {
+                PARTICLETYPE.P_MONEY -> canvas.drawOval(
+                    p.position.x - p.width * 0.5f,
+                    p.position.y - p.height * 0.5f,
+                    p.position.x + p.width * 0.5f,
+                    p.position.y + p.height * 0.5f,
+                    PaintColor.Instance.GetPaint(Color.YELLOW)!!
+                )
+                PARTICLETYPE.P_BLOOD -> p.paint?.let {
+                    canvas.drawOval(
+                        p.position.x - p.width * 0.5f,
+                        p.position.y - p.height * 0.5f,
+                        p.position.x + p.width * 0.5f,
+                        p.position.y + p.height * 0.5f,
+                        it
+                    )
+                }
+                PARTICLETYPE.P_FISH -> {
+                    val transform = Matrix()
+                    val angle = atan2(p.velocity.y, p.velocity.x) * (180f / PI.toFloat()) - 90f
+                    transform.postRotate(angle)
+                    transform.postTranslate(p.position.x, p.position.y)
+                    canvas.drawBitmap(p.GetBMP()!!, transform, null)
+                }
+                else -> canvas.drawBitmap(p.GetBMP()!!, p.position.x - (p.width * 0.5f), p.position.y - (p.height * 0.5f), null)
             }
         }
     }
